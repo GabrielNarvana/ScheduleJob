019fd27ae51bccd0e4a452605e0d116b1c42429a
GabrielNarvana <gabriel.j.l@hotmail.com>
2021-02-15 18:13:56 -0300

    Ajuste de log

--- ModelCronJob/Pool/RoboSchedulado.cs
+++ ModelCronJob/Pool/RoboSchedulado.cs
@@ -21,19 +21,19 @@ namespace ModelCronJob.Pool
 
             public override Task StartAsync(CancellationToken cancellationToken)
             {
-                _logger.LogInformation("CronJob 3 starts.");
+                _logger.LogInformation("CronJob starts.");
                 return base.StartAsync(cancellationToken);
             }
 
             public override Task DoWork(CancellationToken cancellationToken)
             {
-                _logger.LogInformation($"{DateTime.Now:hh:mm:ss} CronJob 3 is working.");
+                _logger.LogInformation($"{DateTime.Now:hh:mm:ss} CronJob is working.");
                 return Task.CompletedTask;
             }
 
             public override Task StopAsync(CancellationToken cancellationToken)
             {
-                _logger.LogInformation("CronJob 3 is stopping.");
+                _logger.LogInformation("CronJob is stopping.");
                 return base.StopAsync(cancellationToken);
             }
     }
